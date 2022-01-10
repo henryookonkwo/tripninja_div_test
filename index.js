1cdf23f98e9ded32fa2e6d99d4cc6c5c84c94fbc
System Administrator <root@Henrys-MacBook-Pro.local>
2022-01-10 11:40:47 -0500

    update alert message

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let div2 = document.getElementById("div2");
 let div3 = document.getElementById("div3");
 
 function handleClick() {
-    alert("The div was clicked is " + this.id);
+    alert("The div that was clicked is " + this.id);
     // console.log(this.id);
   }
 div1.addEventListener("click", handleClick)
